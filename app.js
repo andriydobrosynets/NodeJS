1bc0015d70107fcab1ccc7ae7f79d4f416b09f7f
andriydobrosynets <andriy.andriy1995@gmail.com>
2021-10-01 19:00:10 +0300

    lesson 1

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const fs = require('fs');
 const path = require('path');
+
 const folderMen = path.join(__dirname,'men');
 const folderWomen = path.join(__dirname,'women');
+
 fs.readdir(folderWomen, (err, data) => {
     if (err) {
         console.log(err);
         return;
     }
+
     data.forEach(fileName => {
         fs.readFile(path.join(folderWomen, fileName), (err, data) => {
             if (err) {
                 console.log(err);
                 return;
             }
+
             if (JSON.parse(data.toString()).gender === 'male') {
                 fs.rename(path.join(folderWomen, fileName), path.join(folderMen, fileName), (err) => {
                     if (err) {
@@ -22,21 +26,21 @@ fs.readdir(folderWomen, (err, data) => {
             }
         });
     });
-
 });
-
 
 fs.readdir(folderMen,(err, data) => {
     if (err) {
         console.log(err);
         return;
     }
+
     data.forEach(fileName => {
         fs.readFile(path.join(folderMen, fileName), (err, data) => {
             if (err) {
                 console.log(err);
                 return;
             }
+
             if (JSON.parse(data.toString()).gender === 'female') {
                 fs.rename(path.join(folderMen, fileName), path.join(folderWomen, fileName), (err) => {
                     if (err) {
