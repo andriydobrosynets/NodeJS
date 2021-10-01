ecba0a251e63e33382fb00afd30d9cf4facb068f
andriydobrosynets <andriy.andriy1995@gmail.com>
2021-10-01 19:00:57 +0300

    lesson 1 using the function

--- app_function.js
+++ app_function.js
@@ -1,19 +1,23 @@
 const fs = require('fs');
 const path = require('path');
+
 const folderMen = path.join(__dirname,'men');
 const folderWomen = path.join(__dirname,'women');
+
 function sortPeople (oldFolder,newFolder,gender){
     fs.readdir(oldFolder, (err, data) => {
         if (err) {
             console.log(err);
             return;
         }
+
         data.forEach(fileName => {
             fs.readFile(path.join(oldFolder, fileName), (err, data) => {
                 if (err) {
                     console.log(err);
                     return;
                 }
+
                 if (JSON.parse(data.toString()).gender === gender) {
                     fs.rename(path.join(oldFolder, fileName), path.join(newFolder, fileName), (err) => {
                         if (err) {
@@ -26,5 +30,6 @@ function sortPeople (oldFolder,newFolder,gender){
 
     });
 }
+
 sortPeople(folderWomen,folderMen,'male')
 sortPeople(folderMen,folderWomen,'female')
